60fe1d7e12230361c92ae482fe6dd11b0bb2c0cc
inoue441 <y.inoue@jfd.co.jp>
2020-02-06 08:37:27 +0900

    add Aisatsu

--- src/Aisatsu.php
+++ src/Aisatsu.php
@@ -1,10 +1,22 @@
 <?php
 
+declare(strict_types=1)
 
 namespace Bag2\Hello;
 
 
-class Aisatsu
+final class Aisatsu
 {
+    public function byHour(int $hour): string
+    {
+        if (5 < $hour && $hour < 10) {
+            return 'おはようございま！';
+        }
 
+        if (10 < $hour && $hour < 16) {
+            return 'こんにちはこんにちは！';
+        }
+
+        return 'こんばんは！';
+    }
 }
